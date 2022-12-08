e6b39267fdbe819e8eb038829b18c7ba590811a2
Lenin Capa <leviskp87@gmail.com>
2022-12-08 12:21:29 -0500

    add courses page

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
+import Courses from './pages/courses/courses';
 
 export function App() {
   return (
@@ -13,6 +13,7 @@ export function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/courses" element={<Courses />}></Route>
       </Routes>
     </>
   );

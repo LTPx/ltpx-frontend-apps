f6f148ae67ee3b8d8ffa23bc31485489ff6c67bb
Lenin Capa <leviskp87@gmail.com>
2022-12-21 15:14:13 -0500

    dynamic form contents

--- apps/client/src/app/pages/teacher/course/course-contents/course-contents.tsx
+++ apps/client/src/app/pages/teacher/course/course-contents/course-contents.tsx
@@ -1,24 +1,96 @@
-import { Input } from '@ltpx-frontend-apps/shared-ui';
+import { Button, ColorsButton, FileUpload, Icon, Input } from '@ltpx-frontend-apps/shared-ui';
+import { useState } from 'react';
 import styles from './course-contents.module.scss';
 
 /* eslint-disable-next-line */
+export interface FormContent {
+  title: string;
+  description: string;
+  media?: any;
+}
+
 export interface CourseContentsProps {}
 
 export function CourseContents(props: CourseContentsProps) {
+
+  const contents: FormContent[] = [
+    {
+      title: '',
+      description: '',
+      media: null
+    }
+  ];
+
+  const [contentForms, setContentForms] = useState(contents);
+
+  const addNewForm = () => {
+    setContentForms([...contentForms,     {
+      title: '',
+      description: '',
+      media: null
+    }])
+  }
+
+  const removeForm = (index: number) => {
+    const forms = [...contentForms];
+    forms.splice(index, 1);
+    setContentForms(forms);
+  }
+
+  const handleInputChange = (e: { target: HTMLInputElement; }, index: number) => {
+    const { name, value } = e.target;
+    let forms = [...contentForms];
+    if ( name === 'title') {
+      forms[index][name] = value;
+    }
+    if ( name === 'description') {
+      forms[index][name] = value;
+    }
+    setContentForms(forms);
+
+    // setContentForms((prevState) => (
+    //   {
+    //     ...prevState,
+    //     [e.target.name]: [e.target.value]
+    //   }
+    // ))
+  };
+
   return (
     <div className="contents">
-      <div className={styles['text']}>
+      <div className={styles['header-text']}>
         <h2>Contenidos</h2>
         <h4 className='muted'>Los contenidos que se impartiran en el desarrollo del curso</h4>
       </div>
-      <div className={styles['text']}>
-        <Input placeholder='Ejm: Introduccion' label='Titulo de esta seccion'/>
-        <Input placeholder='Un breve resumen de lo que trata este curso' label='Texto de esta seccion'/>
-        <div className={styles['media']}>
-          <label>Agregar contenido multimedia</label>
+      { contentForms.map((form, index)=> (
+        <div className={styles['form-content']} key={index}>
+          <div className={styles['remove-btn']} onClick={() => removeForm(index)}>
+            <Icon icon='close' size={15}/>
+          </div>
+          <Input
+            placeholder='Ejm: Introduccion'
+            label='Titulo de esta seccion'
+            value={form.title}
+            onChange={(e: { target: HTMLInputElement; }) => handleInputChange(e, index)}
+            name='title'
+          />
+          <Input
+            placeholder='Descripcion de este contenido'
+            label='Descripcion de esta seccion'
+            value={form.description}
+            onChange={(e: { target: HTMLInputElement; }) => handleInputChange(e, index)}
+            name='description'
+          />
+          {/* <div className={styles['media']}>
+            <FileUpload text='Subir archivos' />
+          </div> */}
         </div>
-      </div>
-      <span>+ Agregar otra seccion</span>
+      )) }
+      <Button
+        title='+ Agregar otra seccion'
+        onClick={addNewForm}
+        color={ColorsButton.primary}
+      />
     </div>
   );
 }

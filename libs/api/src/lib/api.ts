13c08f2422d89c34efb135438401a8963121409f
Lenin Capa <leviskp87@gmail.com>
2022-12-27 16:49:43 -0500

    add staging url

--- libs/api/src/lib/api.ts
+++ libs/api/src/lib/api.ts
@@ -1,2 +1,2 @@
 export const LOCAL_API_URL = 'http://localhost:3000';
-export const STAGING_API_URL = 'https://ltpx-client.netlify.app';
+export const STAGING_API_URL = 'https://ltpx-api-staging.up.railway.app';

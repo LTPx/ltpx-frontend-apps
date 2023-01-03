fb25b43a978512c09468288760d9f189da246c8d
Lenin Capa <leviskp87@gmail.com>
2023-01-03 16:39:55 -0500

    add formik to create course

--- apps/client/src/app/pages/teacher/new-course/new-course.tsx
+++ apps/client/src/app/pages/teacher/new-course/new-course.tsx
@@ -1,4 +1,5 @@
-import { Button, ColorsButton, Input, Select, Tabs } from '@ltpx-frontend-apps/shared-ui';
+import { Button, ColorsButton, Input, Select, Tabs, TypeButton } from '@ltpx-frontend-apps/shared-ui';
+import { useFormik } from 'formik';
 import { useState } from 'react';
 import Achievement from '../achievement/achievement';
 import CourseContents from '../course/course-contents/course-contents';
@@ -7,6 +8,7 @@ import CourseSettings from '../course/course-settings/course-settings';
 import Quiz from '../quiz/quiz';
 import TeacherClasses from '../teacher-classes/teacher-classes';
 import styles from './new-course.module.scss';
+import * as Yup from 'yup';
 
 /* eslint-disable-next-line */
 export interface NewCourseProps {}
@@ -28,48 +30,78 @@ const tabs = [
 export function NewCourse(props: NewCourseProps) {
   const [indexViewSelected, setIndexViewSelected] = useState(0);
 
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      category: '',
+      language: '',
+      level: '',
+      goals: '',
+      requirements: '',
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required('es obligatorio'),
+      description: Yup.string().required('es obligatorio'),
+      category: Yup.string().required('es obligatorio'),
+      language: Yup.string().required('es obligatorio'),
+      level: Yup.string().required('es obligatorio'),
+      goals: Yup.string().required('es obligatorio'),
+      requirements: Yup.string().required('es obligatorio'),
+    }),
+    onSubmit: async formData => {
+      console.log(formData);
+    }
+  });
+
   return (
     <div className={styles['container']}>
-      <div className={styles['header']}>
-        <h3>Crear Curso</h3>
-        <div className={styles['actions']}>
-          <h5 className='muted'>Creado: Diciembre 21 2022</h5>
-          <Button title='Preview' outline={true}/>
-          <Select options={optionsSave} />
-          <Button title='Guardar' color={ColorsButton.primary}/>
+      <form onSubmit={formik.handleSubmit}>
+        <div className={styles['header']}>
+          <h3>Crear Curso</h3>
+          <div className={styles['actions']}>
+            <h5 className='muted'>Creado: Diciembre 21 2022</h5>
+            <Button title='Preview' outline={true}/>
+            <Select options={optionsSave} />
+            <Button
+              title='Guardar'
+              color={ColorsButton.primary}
+              type={TypeButton.submit}
+            />
+          </div>
         </div>
-      </div>
-      <div className={styles['content']}>
-        <div className={styles['course-sections']}>
-          <Tabs
-            tabs={tabs}
-            vertical={true}
-            onClickTab={(index)=>{setIndexViewSelected(index)}}
-          />
+        <div className={styles['content']}>
+          <div className={styles['course-sections']}>
+            <Tabs
+              tabs={tabs}
+              vertical={true}
+              onClickTab={(index)=>{setIndexViewSelected(index)}}
+            />
+          </div>
+          <div className={styles['course-section-content']}>
+            <section>
+              { indexViewSelected === 0 && (
+                <CourseGeneralInformation formik={formik}/>
+              )}
+              { indexViewSelected === 1 && (
+                <CourseContents/>
+              )}
+              { indexViewSelected === 2 && (
+                <TeacherClasses/>
+              )}
+              { indexViewSelected === 3 && (
+                <Quiz/>
+              )}
+              { indexViewSelected === 4 && (
+                <Achievement/>
+              )}
+              { indexViewSelected === 5 && (
+                <CourseSettings/>
+              )}
+            </section>
+          </div>
         </div>
-        <div className={styles['course-section-content']}>
-          <section>
-            { indexViewSelected === 0 && (
-              <CourseGeneralInformation/>
-            )}
-            { indexViewSelected === 1 && (
-              <CourseContents/>
-            )}
-            { indexViewSelected === 2 && (
-              <TeacherClasses/>
-            )}
-            { indexViewSelected === 3 && (
-              <Quiz/>
-            )}
-            { indexViewSelected === 4 && (
-              <Achievement/>
-            )}
-            { indexViewSelected === 5 && (
-              <CourseSettings/>
-            )}
-          </section>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }

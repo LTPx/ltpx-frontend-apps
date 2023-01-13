d7d2522eefa0458e07c790f8135d38e9bb748360
ricardokpa <64665611+ricardokpa@users.noreply.github.com>
2023-01-13 15:54:34 -0500

    changes in component login-form

--- apps/client/src/app/pages/site/login/login.tsx
+++ apps/client/src/app/pages/site/login/login.tsx
@@ -2,18 +2,11 @@ import styles from './login.module.scss';
 import {
   BannerNotification,
   BannerType,
-  Button,
-  ColorsButton,
-  Input,
-  InputTextStatus,
-  StatusInputText,
-  TypeButton,
+  LoginForm,
 } from '@ltpx-frontend-apps/shared-ui';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
 import { useState } from 'react';
-import { TypeAccounts } from '@ltpx-frontend-apps/api';
+import { ICredentials, TypeAccounts } from '@ltpx-frontend-apps/api';
 import { useUser } from '../../../store';
 
 /* eslint-disable-next-line */
@@ -22,53 +15,30 @@ export interface LoginProps {}
 export function Login(props: LoginProps) {
   const [error, setError] = useState(false);
   const { login } = useUser();
-  const status = 1;
 
   const navigate = useNavigate();
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: Yup.object().shape({
-      email: Yup.string().email("Invalid email ").required('Email is required'),
-      password: Yup.string().required('Password is required'),
-    }),
-    onSubmit: async (formData) => {
-      const userAccount = {
-        email: formData.email,
-        password: formData.password,
-      };
-      console.log('hola');
-      const { isLogin, data } = await login(userAccount);
-
-      if (isLogin) {
-        if (data.initial_register === TypeAccounts.student) {
-          navigate('/student/dashboard');
-        }
-        if (data.initial_register === TypeAccounts.teacher) {
-          navigate('/teacher/dashboard');
-        }
-        window.location.reload();
-      } else {
-        setError(true);
+  const onSubmitForm = async(formData: ICredentials) => {
+    const userAccount = {
+      email: formData.email,
+      password: formData.password,
+    };
+    const { isLogin, data } = await login(userAccount);
+    if (isLogin) {
+      if (data.initial_register === TypeAccounts.student) {
+        navigate('/student/dashboard');
       }
-    },
-  });
+      if (data.initial_register === TypeAccounts.teacher) {
+        navigate('/teacher/dashboard');
+      }
+      window.location.reload();
+    } else {
+      setError(true);
+    }
+  }
 
   return (
     <div className="main-container">
-      {status === 1 ? (
-        <BannerNotification>
-          Se a enviado un correo de validación;
-        </BannerNotification>
-      ) : (
-        <BannerNotification type={BannerType.error}>
-          correo de validación;
-        </BannerNotification>
-      )}
-
       <div className={styles['container']}>
         <div className={styles['content']}>
           <h1>Iniciar Sesión</h1>
@@ -84,46 +54,7 @@ export function Login(props: LoginProps) {
               Tu email o password no coinciden, prueba recuperando contraseña
             </BannerNotification>
           )}
-          <form onSubmit={formik.handleSubmit}>
-            <Input
-              label="Email"
-              type="email"
-              name="email"
-              placeholder="myemail@example.com"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <InputTextStatus
-                status={StatusInputText.error}
-                text={formik.errors.email}
-              />
-            ) : null}
-            <Input
-              label="Password"
-              type="password"
-              name="password"
-              placeholder="********"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <InputTextStatus
-                status={StatusInputText.error}
-                text={formik.errors.password}
-              />
-            ) : null}
-            <Button
-              className={styles['btn-submit']}
-              color={ColorsButton.primary}
-              title="Iniciar"
-              full={true}
-              type={TypeButton.submit}
-              onClick={formik.handleSubmit}
-            />
-          </form>
+          <LoginForm onSubmit={(formData)=>{onSubmitForm(formData)}} />
           <NavLink to={'/forget-password'} className={styles['link']}>
             Olvidaste tu contraseña?
           </NavLink>
@@ -134,3 +65,13 @@ export function Login(props: LoginProps) {
 }
 
 export default Login;
+
+// {status === 1 ? (
+//   <BannerNotification>
+//     Se a enviado un correo de validación;
+//   </BannerNotification>
+// ) : (
+//   <BannerNotification type={BannerType.error}>
+//     correo de validación;
+//   </BannerNotification>
+// )}

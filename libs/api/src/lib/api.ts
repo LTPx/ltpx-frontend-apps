21e23a4a98477c7a337d799aefb732e19f8654a2
Lenin Capa <leviskp87@gmail.com>
2023-01-16 19:56:19 -0500

    update host

--- libs/api/src/lib/api.ts
+++ libs/api/src/lib/api.ts
@@ -2,7 +2,7 @@ export const LOCAL_API_URL = 'http://localhost:3000';
 export const STAGING_API_URL = 'https://ltpx-api-staging.up.railway.app';
 
 export const getApiUrl = () => {
-  if (window.location.host.includes('ltpx-client.netlify.app')) {
+  if (window.location.host.includes('staging.growopenminds.com')) {
     return STAGING_API_URL;
   }
   return LOCAL_API_URL;

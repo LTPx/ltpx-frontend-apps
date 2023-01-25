a23b73e62e0e05d196c6b86eb2acbb8d6f2526ff
ricardokpa <64665611+ricardokpa@users.noreply.github.com>
2023-01-25 13:11:28 -0500

    header-responsive

--- libs/shared-ui/src/lib/header/header.tsx
+++ libs/shared-ui/src/lib/header/header.tsx
@@ -1,6 +1,8 @@
 import styles from './header.module.scss';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import Brand from '../brand/brand';
+import { useState } from 'react';
+import Icon from '../icon/icon';
 
 /* eslint-disable-next-line */
 
@@ -17,28 +19,53 @@ export interface HeaderProps {
 
 export function Header(props: HeaderProps) {
   const { children, links, className } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className={`${styles['container']} ${className}`}>
+    <div className={`${styles['container-header']} ${className}`}>
       <div className={styles['main-action']}>
-        <Brand/>
-        <div className={styles['search']}>
-        </div>
-      </div>
+        <Brand />
+      </div> 
       <div className={styles['information']}>
         <div className={styles['links']}>
-          { links.map((link, index) => (
-              <NavLink
-                key={index}
-                className={({ isActive }) =>
-                isActive ? `${styles['active-link']} ${styles['link']} ${link.accent ? styles['accent'] : ''}` : `${styles['link']} ${link.accent ? styles['accent'] : ''}`
+          {links.map((link, index) => (
+            <NavLink
+              key={index}
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles['active-link']} ${styles['link']} ${
+                      link.accent ? styles['accent'] : ''
+                    }`
+                  : `${styles['link']} ${link.accent ? styles['accent'] : ''}`
               }
-                to={link.url}
-              >
-                <h4 className={`${styles['link']}`} >{link.title}</h4>
-              </NavLink>
+              to={link.url}
+            >
+              <h4 className={`${styles['link']}`}>{link.title}</h4>
+            </NavLink>
           ))}
         </div>
         {children}
+      </div>
+
+      <div className={styles['header-small']}>
+        <div className={styles['panel-container']}>
+          <div className={styles['panel-open']} onClick={handleClick}>
+            {isOpen === false ? (
+              <Icon icon={'menu'} size={40}></Icon>
+            ) : (
+              <Icon icon={'minus'} size={40}></Icon>
+            )}
+          </div>
+          <div className={`${styles['panel']} ${isOpen ? styles['open'] : ''}`}>
+            {links.map((link, index) => (
+              <NavLink to={link.url}>
+                <h4 className={`${styles['link-options']}`}>{link.title}</h4>
+              </NavLink>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

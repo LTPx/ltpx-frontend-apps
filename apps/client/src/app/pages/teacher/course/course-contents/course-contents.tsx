9e0aa0adb6424d373cb8883216c759e5310b2166
Lenin Capa <leviskp87@gmail.com>
2023-01-29 22:03:07 -0500

    fix not catch 0 index

--- apps/client/src/app/pages/teacher/course/course-contents/course-contents.tsx
+++ apps/client/src/app/pages/teacher/course/course-contents/course-contents.tsx
@@ -98,7 +98,7 @@ export function CourseContents(props: CourseContentsProps) {
           }}
           onSubmit={(content) => {
             setOpenModal(false);
-            if (indexContentEdit) {
+            if (indexContentEdit !== undefined) {
               updateContent(content, indexContentEdit);
             } else {
               addNewContent(content);

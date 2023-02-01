2583ced5c047c7b035294ba9c697310587e3ac03
Lenin Capa <leviskp87@gmail.com>
2023-02-01 18:06:48 -0500

    add header actions buttons

--- apps/admin/src/app/pages/course-details-page/course-details-page.tsx
+++ apps/admin/src/app/pages/course-details-page/course-details-page.tsx
@@ -1,7 +1,9 @@
 import { CLASSROOMS } from '@ltpx-frontend-apps/api';
 import {
   AchievementsList,
+  Button,
   ClassroomView,
+  ColorsButton,
   CourseContents,
   InformationCard,
   OverviewCourse,
@@ -42,65 +44,89 @@ export function CourseDetailsPage() {
       fetchData();
     }
   }, []);
+
+  const handleApproveCourse = () => {
+
+  }
+
   return (
     <div className={styles['container']}>
       {viewCourse && (
-        <>
-          <div className={styles['cover']}>
-            <img src={viewCourse.cover_url}></img>
-          </div>
-          <h1>{viewCourse.title}</h1>
-          <div className={styles['basic-info']}>
-            <span className={`${styles['noted']}`}>
-              {translateCategory(viewCourse.category)}
-            </span>
-            <span className={`${styles['noted']}`}>
-              {translateLevel(viewCourse.level)}
-            </span>
-            <span className={`${styles['noted']}`}>
-              {translateLanguage(viewCourse.language)}
-            </span>
-          </div>
-          <Tabs
-            tabs={tabs}
-            isNav={false}
-            onClickTab={(index) => setSelectedTab(index)}
-          />
-          <div className={styles['course-content']}>
-            {selectedTab === 0 && (
-              <OverviewCourse
-                description={viewCourse.description}
-                goals={viewCourse.learn_goals?.split('\n') || []}
-                requirements={viewCourse.requirements?.split('\n') || []}
+        <div className="c">
+          <div className={styles['header']}>
+            <h1>{viewCourse.title}</h1>
+            <div className={styles['actions']}>
+              <Button
+                title="Requiere cambios"
+                color={ColorsButton.secondary}
+                outline={true}
+                onClick={() => {
+                  // handleRequestChange();
+                }}
               />
-            )}
-            {selectedTab === 1 && (
-              <CourseContents contents={contents || []} />
-            )}
-            {selectedTab === 2 && (
-              <QuizzesList quizzes={quizzes || []} />
-            )}
-            {selectedTab === 3 && (
-              <AchievementsList achievements={achievements || []} />
-            )}
-            {selectedTab === 4 && (
-              <>
-                <InformationCard
-                  title={CLASSROOMS[classroom.condition].title}
-                  text={CLASSROOMS[classroom.condition].text}
-                  icon={CLASSROOMS[classroom.condition].icon}
-                  selected={true}
-                />
-                {classroom.meetings.length > 0 && (
-                  <ClassroomView
-                    classroom={classroom}
-                    className={styles['classroom-summary']}
-                  />
-                )}
-              </>
-            )}
+              <Button
+                title="Aprobar curso"
+                onClick={() => {
+                  handleApproveCourse();
+                }}
+              />
+            </div>
           </div>
-        </>
+
+          <div>
+            <div className={styles['cover']}>
+              <img src={viewCourse.cover_url}></img>
+            </div>
+            <div className={styles['basic-info']}>
+              <span className={`${styles['noted']}`}>
+                {translateCategory(viewCourse.category)}
+              </span>
+              <span className={`${styles['noted']}`}>
+                {translateLevel(viewCourse.level)}
+              </span>
+              <span className={`${styles['noted']}`}>
+                {translateLanguage(viewCourse.language)}
+              </span>
+            </div>
+            <Tabs
+              tabs={tabs}
+              isNav={false}
+              onClickTab={(index) => setSelectedTab(index)}
+            />
+            <div className={styles['course-content']}>
+              {selectedTab === 0 && (
+                <OverviewCourse
+                  description={viewCourse.description}
+                  goals={viewCourse.learn_goals?.split('\n') || []}
+                  requirements={viewCourse.requirements?.split('\n') || []}
+                />
+              )}
+              {selectedTab === 1 && (
+                <CourseContents contents={contents || []} />
+              )}
+              {selectedTab === 2 && <QuizzesList quizzes={quizzes || []} />}
+              {selectedTab === 3 && (
+                <AchievementsList achievements={achievements || []} />
+              )}
+              {selectedTab === 4 && (
+                <>
+                  <InformationCard
+                    title={CLASSROOMS[classroom.condition].title}
+                    text={CLASSROOMS[classroom.condition].text}
+                    icon={CLASSROOMS[classroom.condition].icon}
+                    selected={true}
+                  />
+                  {classroom.meetings.length > 0 && (
+                    <ClassroomView
+                      classroom={classroom}
+                      className={styles['classroom-summary']}
+                    />
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );

16d8085eb7c2a0b2c0de95d5c6efa3d7582d8f82
ricardokpa <64665611+ricardokpa@users.noreply.github.com>
2023-02-08 19:23:12 -0500

    add page category (#52)

---
+++ apps/client/src/app/pages/site/courses-by-category/courses-by-category.tsx
@@ -0,0 +1,51 @@
+import { buildCourses } from '@ltpx-frontend-apps/api';
+import { CourseCard, InputSearch } from '@ltpx-frontend-apps/shared-ui';
+import { useTranslation } from 'react-i18next';
+import { NavLink, useParams } from 'react-router-dom';
+import styles from './courses-by-category.module.scss';
+
+/* eslint-disable-next-line */
+export interface CoursesByCategoryProps {}
+
+export function CoursesByCategory(props: CoursesByCategoryProps) {
+  const popularCourses = buildCourses(4);
+  const { t } = useTranslation();
+  const { categoryId } = useParams();
+  return (
+    <div className={styles['container']}>
+      <div className={styles['content']}>
+        <div className={styles['title']}>
+          <h1>{t(`course_categories.${categoryId}`)}</h1>
+          <div className={styles['link-browser']}>
+            <NavLink to="/courses">{t('links.toAllCourses')}</NavLink>
+          </div>
+        </div>
+        <div className={styles['search-course']}>
+          <h4>Se muestran 4 cursos disponibles para ti</h4>
+          <InputSearch
+            className={styles['search-responsive']}
+            placeholder="Buscar cursos"
+          />
+        </div>
+        <div className={styles['courses-by-category']}>
+          {popularCourses.map((course, index) => (
+            <div className={styles['course']} key={index}>
+              <CourseCard
+                image={course.cover}
+                category={course.category}
+                title={course.title}
+                price={course.price_cents}
+                duration={0}
+                lessons={0}
+                stars={course.average_rating}
+                link={`/course/${course.id}/details`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CoursesByCategory;

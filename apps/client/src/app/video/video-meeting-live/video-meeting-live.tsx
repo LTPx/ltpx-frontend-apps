c347824668be639be493aadfe5e2543f87b909bf
Lenin Capa <leviskp87@gmail.com>
2023-02-15 19:24:11 -0500

    fixing type issues

--- apps/client/src/app/video/video-meeting-live/video-meeting-live.tsx
+++ apps/client/src/app/video/video-meeting-live/video-meeting-live.tsx
@@ -1,13 +1,43 @@
-import VideoMeeting from '../video-meeting/video-meeting';
+// import VideoMeeting from '../video-meeting/video-meeting';
 import styles from './video-meeting-live.module.scss';
+import { VideoSDKMeeting } from "@videosdk.live/rtc-js-prebuilt";
+// import * as VideoSDKMeeting from "@videosdk.live/rtc-js-prebuilt/dist";
 
+import { useEffect } from 'react';
 /* eslint-disable-next-line */
 export interface VideoMeetingLiveProps {}
 
 export function VideoMeetingLive(props: VideoMeetingLiveProps) {
+  useEffect(() => {
+    const config = {
+      name: "Demo User",
+      meetingId: "milkyway",
+      apiKey: "<API KEY>",
+
+      containerId: null,
+
+      micEnabled: true,
+      webcamEnabled: true,
+      participantCanToggleSelfWebcam: true,
+      participantCanToggleSelfMic: true,
+
+      chatEnabled: true,
+      screenShareEnabled: true,
+
+      /*
+
+     Other Feature Properties
+
+      */
+    };
+
+    const meeting = new VideoSDKMeeting();
+    meeting.init(config);
+  }, []);
+
   return (
     <div className={styles['container']}>
-      <VideoMeeting/>
+      {/* <VideoMeeting/> */}
     </div>
   );
 }

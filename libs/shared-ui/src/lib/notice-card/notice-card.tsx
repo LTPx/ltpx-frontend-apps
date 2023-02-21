15705d4f09ac6f98e38ca983ad913a515bef4aa6
ricardokpa <64665611+ricardokpa@users.noreply.github.com>
2023-02-21 12:34:51 -0500

    remove

--- libs/shared-ui/src/lib/notice-card/notice-card.tsx
+++ libs/shared-ui/src/lib/notice-card/notice-card.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react';
-import Button, { ColorsButton } from '../button/button';
 import styles from './notice-card.module.scss';
 
 /* eslint-disable-next-line */
@@ -7,15 +6,15 @@ export interface NoticeCardProps {
   image?: string;
   title: string;
   description: string;
-  children?: ReactElement; //este debe ser el boton
-  className?: string; //aqui le podemos pasar el color de fondo del card
+  children?: ReactElement;
+  className?: string;
 }
 
 export function NoticeCard(props: NoticeCardProps) {
   const { image, title, description, children, className } = props;
   return (
     <div className={`${styles['content']} ${className}`}>
-      {image && (<img src={image} />)}
+      {image && <img src={image} />}
       <h2>{title}</h2>
       <p>{description}</p>
       {children}
@@ -24,19 +23,3 @@ export function NoticeCard(props: NoticeCardProps) {
 }
 
 export default NoticeCard;
-
-{
-  /* <div className={styles['card-teaching']}>
-        <h2>Enseñar en Openmind</h2>
-        <p>
-          Revisa nuestra blog donde encontraras todo la información de como
-          funciona Openmind
-        </p>
-        <Button
-          title="Aprender Mas"
-          color={ColorsButton.secondary}
-          full={true}
-          link="/teacher/blog"
-        />
-      </div> */
-}

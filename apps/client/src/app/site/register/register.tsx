03669e3318e672cbc6d21f1b80687a393c1ede11
Lenin Capa <leviskp87@gmail.com>
2023-03-21 12:20:41 -0500

    remove new changes

--- apps/client/src/app/site/register/register.tsx
+++ apps/client/src/app/site/register/register.tsx
@@ -27,30 +27,6 @@ export function Register(props: RegisterProps) {
   return (
     <div className={styles['main-container']}>
       <div className={styles['container']}>
-        <div className={styles['information']}>
-          <div className={styles['wrap-information']}>
-            <img
-              className={styles['img-register']}
-              src="../../../../assets/images/vector.svg"
-              alt="img-register"
-            />
-            <div className={styles['information-register']}>
-              <h2 className={styles['title-register']}>
-                Bienvenido a OpenMind
-              </h2>
-              <p className={styles['text-register']}>
-                No estarás obligado a ir al ritmo del más lento ni a aprender
-                cosas inútiles. Tu representante sólo pagará al docente una vez
-                que cumplas tus objetivos motivando de esa manera al docente a
-                enfocarse en lo que más importa; tu éxito. 
-                {/* Podrás ver tus
-                objetivos desde el inicio y completarlos a tu propio ritmo. Una
-                vez que los completes, puedes avanzar de inmediato al siguiente
-                nivel. */}
-              </p>
-            </div>
-          </div>
-        </div>
         <div className={styles['register']}>
           <div className={styles['register-content']}>
             <h1 className={styles['title']}>{t('registerForm.user.title')}</h1>

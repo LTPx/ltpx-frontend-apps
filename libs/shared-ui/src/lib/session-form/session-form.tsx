7793154305369435f759d156c0b6931d039a9b13
Lenin Capa <leviskp87@gmail.com>
2023-03-28 18:42:01 -0500

    remove old component move to new component

--- libs/shared-ui/src/lib/session-form/session-form.tsx
+++ libs/shared-ui/src/lib/session-form/session-form.tsx
@@ -1,8 +1,7 @@
 import Drawer from '../drawer/drawer';
-import { useFormik } from 'formik';
+import { FieldArray, Form, Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import Button, { ColorsButton, TypeButton } from '../button/button';
-import SelectDates from '../select-dates/select-dates';
 import Select from '../select/select';
 import styles from './session-form.module.scss';
 import Input from '../input/input';
@@ -12,6 +11,9 @@ import {
   TeacherClassType,
 } from '@ltpx-frontend-apps/api';
 import { useUser } from '@ltpx-frontend-apps/store';
+import * as Yup from 'yup';
+import moment from 'moment';
+import Icon from '../icon/icon';
 
 export interface SessionFormProps {
   open?: boolean;
@@ -30,39 +32,24 @@ export function SessionForm(props: SessionFormProps) {
 
   const getFormatDates = () => {
     if (session && session.meetings) {
-      return session.meetings.map((meeting)=>{
+      return session.meetings.map((meeting) => {
         return {
           id: meeting.id,
-          date: meeting.start_date
+          date: meeting.start_date,
         };
       });
     } else {
-      return [{date: today}];
+      return [{ date: today }];
     }
-  }
+  };
 
+  const initialValues = {
+    max_participants: session?.max_participants || 1,
+    hour: 0,
+    minutes: session?.call_time_min || 30,
+    dates: getFormatDates(),
+  };
 
-  const dates = getFormatDates();
-
-  const formik = useFormik({
-    initialValues: {
-      max_participants: '',
-      hour: '0',
-      minutes: '30',
-      dates: [],
-    },
-    onSubmit: (data) => {
-      const formData = {
-        private_sessions: false,
-        max_participants: parseInt(data.max_participants),
-        call_time_min: parseInt(data.minutes) + parseInt(data.hour),
-        meetings_attributes: data.dates.map((date) => {
-          return { start_date: date, host_user_id: user.id };
-        }),
-      };
-      onSubmit && onSubmit(formData);
-    },
-  });
   const hours = [
     { value: '0', text: '0' },
     { value: '60', text: '1' },
@@ -85,69 +72,119 @@ export function SessionForm(props: SessionFormProps) {
         onClose && onClose();
       }}
     >
-      <form className={styles['form']}>
-        <section className={styles['section']}>
-          <h2>Configurar Sesiones</h2>
-          <div className={styles['field-form']}>
-            <Input
-              label="Numero de estudiantes"
-              description="El numero máximo de personas que pueden tomaran este curso"
-              type="number"
-              name="max_participants"
-              placeholder="12 estudiantes recomendado"
-              onChange={(e: any) => {
-                formik.handleChange(e);
-              }}
-              value={formik.values.max_participants}
-              onBlur={formik.handleBlur}
-            />
-          </div>
-          <div className={styles['field-form']}>
-            <label>{t('classroomForm.durationClasses')}</label>
-            <div className={styles['range']}>
-              Cada clase durara{' '}
-              <Select
-                options={hours}
-                onChange={(e) => {
-                  formik.setFieldValue('hour', e.value);
-                }}
-              />{' '}
-              hora(s), con
-              <Select
-                options={minutes}
-                onChange={(e) => {
-                  formik.setFieldValue('minutes', e.value);
-                }}
-              />{' '}
-              minutos
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          const formData = {
+            private_sessions: false,
+            max_participants: values.max_participants,
+            call_time_min: values.minutes + values.hour,
+            meetings_attributes: values.dates.map((date) => {
+              return { start_date: date, host_user_id: user.id };
+            }),
+          };
+          // onSubmit && onSubmit(formData);
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          setFieldValue,
+          submitForm,
+          errors,
+        }) => (
+          <Form className={styles['form-quiz-options']}>
+            <div className={styles['fields']}>
+              <section className={styles['section']}>
+                <h2>Configurar Sesiones</h2>
+                <div className={styles['field-form']}>
+                  <Input
+                    label="Numero de estudiantes"
+                    description="El numero máximo de personas que pueden tomaran este curso"
+                    type="number"
+                    name="max_participants"
+                    placeholder="12 estudiantes recomendado"
+                    onChange={handleChange}
+                    value={values.max_participants}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                <div className={styles['field-form']}>
+                  <label>{t('classroomForm.durationClasses')}</label>
+                  <div className={styles['range']}>
+                    Cada clase durara{' '}
+                    <Select
+                      options={hours}
+                      onChange={(e) => {
+                        setFieldValue('hour', e.value);
+                      }}
+                    />{' '}
+                    hora(s), con
+                    <Select
+                      options={minutes}
+                      onChange={(e) => {
+                        setFieldValue('minutes', e.value);
+                      }}
+                    />{' '}
+                    minutos
+                  </div>
+                </div>
+              </section>
+              <br />
+              <label>{t('classroomForm.dateClasses')}</label>
+              <FieldArray
+                name="dates"
+                render={(arrayHelpers) => (
+                  <div>
+                    {values.dates.map((item, index) => (
+                      <div key={index}>
+                        <Input
+                          type="datetime-local"
+                          name={`dates[${index}].date`}
+                          value={moment(item.date).format('YYYY-MM-DDTHH:mm')}
+                          onChange={handleChange}
+                        />
+                        <div className={styles['actions']}>
+                          <div
+                            className={styles['remove']}
+                            onClick={() => arrayHelpers.remove(index)}
+                          >
+                            <Icon icon="trash" size={15} />
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                    <div
+                      className={styles['add-new']}
+                      onClick={() =>
+                        arrayHelpers.push({date: today})
+                      }
+                    >
+                      + Nueva Fecha
+                    </div>
+                  </div>
+                )}
+              />
             </div>
-          </div>
-          <div className={styles['field-form']}>
-            <label>{t('classroomForm.dateClasses')}</label>
-            <SelectDates
-              onChange={(dates) => {
-                formik.setFieldValue('dates', dates);
-              }}
-              dateItems={dates}
-            />
-          </div>
-        </section>
-        <div className={styles['buttons']}>
-          <Button
-            color={ColorsButton.white}
-            title="Cancelar"
-            onClick={() => {
-              onClose && onClose();
-            }}
-          />
-          <Button
-            color={ColorsButton.primary}
-            title="Guardar"
-            type={TypeButton.submit}
-            onClick={formik.handleSubmit}
-          />
-        </div>
-      </form>
+            <div className={styles['buttons']}>
+              <Button
+                color={ColorsButton.white}
+                title="Cancelar"
+                onClick={() => {
+                  onClose && onClose();
+                }}
+              />
+              <Button
+                color={ColorsButton.primary}
+                title="Guardar"
+                type={TypeButton.submit}
+                onClick={submitForm}
+              />
+            </div>
+          </Form>
+        )}
+      </Formik>
     </Drawer>
   );
 }

db0e971785fa59232a2ac68e5a366273925a323b
Lenin Capa <leviskp87@gmail.com>
2023-04-05 11:41:18 -0500

    send file from task teacher

--- libs/api/src/lib/api/teacher/teacher-tasks-api.ts
+++ libs/api/src/lib/api/teacher/teacher-tasks-api.ts
@@ -1,12 +1,16 @@
 import { _http } from '../../http';
 import { NewTaskParams, TaskModel } from '../../interfaces/task-interface';
+import { encapsuleInFormData } from '../../utils';
 
 const http = _http;
 
 export const createTask = async (courseId: number, params: NewTaskParams) => {
+  const data = encapsuleInFormData(params, {
+    mediaKeys: ['file'],
+  });
   return new Promise<TaskModel>((resolve, reject) => {
     http
-      .post(`api/v1/teacher/courses/${courseId}/tasks`, params)
+      .post(`api/v1/teacher/courses/${courseId}/tasks`, data)
       .then((response) => {
         resolve(response.data);
       })

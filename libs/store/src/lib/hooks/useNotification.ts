7597e88c7418fb28ed41acc14d1416a7e3fa28f7
Lenin Capa <leviskp87@gmail.com>
2023-04-21 17:20:00 -0500

    remove actioncable

--- libs/store/src/lib/hooks/useNotification.ts
+++ libs/store/src/lib/hooks/useNotification.ts
@@ -1,33 +1,42 @@
 import { useEffect } from 'react';
 import { useUser } from '@ltpx-frontend-apps/store';
-import ActionCable from 'actioncable';
-import { NotificationModel, NotificationWebHook } from '@ltpx-frontend-apps/api';
-import { useMoment } from '@ltpx-frontend-apps/shared-ui';
+import { NotificationWebHook } from '@ltpx-frontend-apps/api';
 
 export const useNotification = () => {
   const { user, addNotification, notifications } = useUser();
 
   useEffect(() => {
-    const cable = ActionCable.createConsumer('ws://localhost:3000/cable');
-    const notificationsChannel = cable.subscriptions.create({
-      channel: 'NotificationsChannel',
-      id: user.id
-    }, {
-      received(notification: NotificationWebHook) {
-        console.log('newNotification: ', notification);
-        const newNotification = {
-          user_id: user.id,
-          kind: notification.type,
-          text: notification.text,
-          meta: notification.meta.data,
-          created_at: notification.created_at
-        }
-        addNotification(newNotification);
-      },
-    });
+    const ws = new WebSocket('ws://localhost:3000/cable');
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          command: "subscribe",
+          identifier:  JSON.stringify({
+            channel: 'NotificationsChannel',
+            id: user.id
+          })
+        })
+      )
+    }
+    ws.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      if(data.type === "ping") return;
+      if(data.type === "welcome") return;
+      if(data.type === "confirm_subscription") return;
 
+      const { message } = data;
+      const notification: NotificationWebHook = message;
+      debugger
+      const newNotification = {
+        user_id: user.id,
+        kind: notification.type,
+        text: notification.text,
+        meta: notification.meta.data,
+        created_at: notification.created_at
+      }
+      addNotification(newNotification);
+    }
     return () => {
-      cable.subscriptions.remove(notificationsChannel);
     };
   }, []);
 

d8bb0ba2d3ecef4012e1d65df0ecced5de74e5fe
Lenin Capa <leviskp87@gmail.com>
2023-04-29 18:10:29 -0500

    allow click on notifications

--- apps/client/src/app/components/notifications/notifications.tsx
+++ apps/client/src/app/components/notifications/notifications.tsx
@@ -8,9 +8,13 @@ import {
 import styles from './notifications.module.scss';
 import { useCallback, useEffect, useState } from 'react';
 import { useUser } from '@ltpx-frontend-apps/store';
-import { NotificationModel } from '@ltpx-frontend-apps/api';
+import {
+  NotificationMeta,
+  NotificationModel,
+} from '@ltpx-frontend-apps/api';
 import { messaging } from '../../../firebase';
 import { getToken } from 'firebase/messaging';
+import { NavLink } from 'react-router-dom';
 
 export function Notifications() {
   const {
@@ -39,7 +43,9 @@ export function Notifications() {
 
   async function fetchToken() {
     try {
-      const token = await getToken(messaging, {vapidKey: process.env.NX_FIREBASE_VAPID_KEY});
+      const token = await getToken(messaging, {
+        vapidKey: process.env.NX_FIREBASE_VAPID_KEY,
+      });
       if (token) {
         await _setTokenDevice(token);
       }
@@ -47,7 +53,6 @@ export function Notifications() {
       console.log('An error occurred while retrieving token. ', error);
     }
   }
-
 
   useEffect(() => {
     fetchNotifications();
@@ -66,11 +71,39 @@ export function Notifications() {
       const icon = icons[notification.kind];
       return {
         kind: notification.kind,
-        text: notification.text,
+        text: buildTextLink(notification.text, notification.meta),
         date: fromNow(notification.created_at),
         icon: icon,
       };
     });
+  }
+
+  function buildTextLink(text: string, meta: NotificationMeta) {
+    const links = {
+      fill_application: '/teacher/apply-teach',
+      application_require_change: '/teacher/apply-teach',
+      application_approved: '/teacher/dashboard',
+      course_need_changes: '/teacher/courses/all',
+      course_approved: '/teacher/courses/all',
+      new_student: `/teacher/courses/${meta.entity_id}/students`,
+      task_need_review: `/teacher/courses/${meta.entity_id}/students`,
+      quiz_need_review: `/teacher/courses/${meta.entity_id}/students`,
+      add_credit: '/teacher/earnings',
+      withdrawal_completed: '/teacher/earnings',
+      student_enrolled: '/student/dashboard',
+      class_started: '/student/classes',
+      task_reviewed: `/student/course/${meta.entity_meta.course_slug}`,
+      quiz_reviewed: `/student/course/${meta.entity_meta.course_slug}`,
+      achievement_reached: `/student/course/${meta.entity_meta.course_slug}`,
+      course_completed: `/student/course/${meta.entity_meta.course_slug}`,
+      chat_unread_messages: '/student/dashboard',
+    };
+    const linkToDetails = links[meta.action];
+    return (
+      <div className="text">
+        {text} <NavLink to={linkToDetails}> Da clic aquí para revisar</NavLink>
+      </div>
+    );
   }
 
   async function clearNotifications() {
@@ -85,10 +118,7 @@ export function Notifications() {
   return (
     <Dropdown>
       <NotificationList notifications={notificationsItems} />
-      <div
-        className={styles['avatar']}
-        onClick={clearNotifications}
-      >
+      <div className={styles['avatar']} onClick={clearNotifications}>
         <Cart amount={totalUnreadNotifications} />
       </div>
     </Dropdown>

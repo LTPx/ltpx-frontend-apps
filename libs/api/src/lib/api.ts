6faee023f4f18046892d3ae8d6377fbe6e04255a
Lenin Capa <leviskp87@gmail.com>
2023-05-29 21:22:01 -0500

    update domain

--- libs/api/src/lib/api.ts
+++ libs/api/src/lib/api.ts
@@ -2,7 +2,7 @@ export const LOCAL_API_URL ='http://localhost:3000';
 export const STAGING_API_URL = 'https://growopenmind-api.fly.dev';
 
 const clientUrl = 'growopendminds.netlify.app';
-const adminUrl = 'admin-growopenminds.netlify.app';
+const adminUrl = 'admin-system-growopenminds.netlify.app';
 
 export const getApiUrl = () => {
   if (window.location.host.includes(clientUrl) || window.location.host.includes(adminUrl) ) {
